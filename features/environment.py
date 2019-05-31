beac334b4c4f36ad5b4c4848362b6b09ab7715e6
ricardov <rvcantillo@gmail.com>
2019-05-31 10:17:41 -0500

    Save image using context menu

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
+from pynput.keyboard import Controller
 
 
 def before_all(context):
@@ -15,6 +17,8 @@ def before_scenario(context, scenario):
     context.driver = webdriver.Chrome(ChromeDriverManager().install())
     context.driver.maximize_window()
     context.driver.implicitly_wait(60)
+    context.action = ActionChains(context.driver)
+    context.keyboard = Controller()
 
 
 def after_scenario(context, scenario):
--- features/steps/search_image.py
+++ features/steps/search_image.py
@@ -1,23 +1,155 @@
 from behave import *
-import urllib.request
-
-
+from pynput.keyboard import Key
+import time
 @given("I open google images")
 def open_google_image(context):
+
     context.driver.get("https://www.google.com.co/imghp")
 
 
 @when('I search any "{word}" on google')
 def search_google(context, word):
+
     context.driver.find_element_by_name("q").send_keys(word)
     context.driver.find_element_by_css_selector("svg").click()
 
 
 @then('I choose the image on the position "{number}"')
 def choose_image(context, number):
-    context.img = context.driver.find_element_by_xpath("//div[@id='rg_s']/div[" + number + "]/a/img").get_attribute("src")
+
+    context.img = context.driver.find_element_by_xpath("//div[@id='rg_s']/div[" + number + "]/a/img")
 
 
 @then('I save the image with desire file "{name}"')
 def save_image(context, name):
-    urllib.request.urlretrieve(context.img, "images/" + name + ".png")
+
+    context.action.context_click(context.img).perform()
+    context.keyboard.press("v")
+    context.keyboard.release("v")
+    # wait for save window
+    time.sleep(1)
+    context.keyboard.type(name)
+    context.keyboard.press(Key.enter)
+    context.keyboard.release(Key.enter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
